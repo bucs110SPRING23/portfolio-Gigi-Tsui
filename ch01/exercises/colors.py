e99064340a193d003af585f8b4ec1d17050f1f8c
Gigi Tsui <gigitsui@Gigis-MacBook-Air.local>
2023-02-01 19:00:25 -0500

    mycolors

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,11 @@
+mycolors = ["red", "orange", "green", "purple", "blue"]
+
+list_size = len(mycolors)
+print ("I have", (list_size), "colors")
+
+print(mycolors[0])
+print(mycolors[1])
+print(mycolors[2])
+print(mycolors[3])
+print(mycolors[4])
+
