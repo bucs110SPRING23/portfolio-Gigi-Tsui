76eaa945618bc9a235f923b2b66ff8174cf50754
Gigi <gtsui1@binghamton.edu>
2023-02-22 17:04:16 -0500

    guess

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,24 +1,13 @@
 import random
-import pygame
-correct_answer = print(random.randint(1,10))
 
-user_guess = input("Guess a number between 1 and 10")
-guess = int(user_guess)
+num = print(random.randint(1,10))
 
+for _ in range(3):
+    guess = int(input("Guess a number between 1 and 10: "))
 
-guess = ""
-if guess == user_guess:
-    print ("Correct! You Win.")
-    pygame.quit()
-elif guess < user_guess:
-    print ("the number is higher,")
-else:
-    print("The number is lower")
-
-limit = 3
-for i in range(limit):
-    if (i == limit - 1, limit == 0):
-        print ("game over")
-
-
-pygame.quit()
+    if guess == num: 
+        print ("Correct! You Win. My number was", num)
+    elif guess < num:
+        print ("The number is lower")
+    elif guess > num:
+        print ("the number is higher,")
