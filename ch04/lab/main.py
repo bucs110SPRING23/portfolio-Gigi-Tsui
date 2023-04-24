2b3277fe17188e0623f4fb7763a4a460cd43a51d
Gigi <gtsui1@binghamton.edu>
2023-04-24 11:40:37 -0400

    class notes

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 import math
 
+#part A
 pygame.init()
 pygame.event.get()
 screen = pygame.display.set_mode((700, 700))
@@ -13,17 +15,21 @@ pygame.draw.circle(screen, "red", (center, center), 340)
 pygame.display.flip()
 pygame.time.wait(1000)
 
+
+
+#part B
 import random
 
-player1 = 
-player2 = 
+# distance_from_center = math.hypot (x1-x2, y1-y2) #the distance formula
+# is_in_circle = distance_from_center <= width/2 #screen width
+#loop that throws 10 random dartw in the board
 
-for i in range (20):
+for i in range (10):
     dart_y = random.randrange(0, 700)
     dart_x = random.randrange(0, 700)
     distance_from_center = math.hypot(center-dart_x, center-dart_y) #the distance formula
     # is_in_circle = distance_from_center <= width/2 #screen width
-    if distance_from_center <= 700/2:
+    if distance_from_center <= 340:
         is_in_circle = True
         pygame.draw.circle(screen, "green", (dart_x, dart_y), 10)
     else:
@@ -32,9 +38,6 @@ for i in range (20):
 
     pygame.display.flip()
     pygame.time.wait(1000)
-
-
-while is_in_circle = True
 
 pygame.display.quit()
 
---
+++ ch08/exercises/something/controller.py
@@ -0,0 +1,20 @@
+class Controller:
+    def __init__(self):
+        #...
+        self.snowpeoples = pygame.sprite.Group()
+        self.max_snowmen = 20
+        #...
+
+    number_of_starting_snowpeople = 2
+interval = self.width/(number_of_starting_snowpeople+1)
+xpos = interval
+for _ in range(number_of_starting_snowpeople):
+    new_sm = Snowman(xpos, self.height/2)
+    self.snowpeoples.add(new_sm)
+    xpos += interval
+
+
+for_ in range (number of people 
+               s=snowman(x,self.screen)
+
+def mainloop
