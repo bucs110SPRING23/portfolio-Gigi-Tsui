5e865995daab4a411039fbfaa7faff492d40dcb6
Gigi <gtsui1@binghamton.edu>
2023-04-27 21:21:04 -0400

    lab 5 redo

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -2,13 +2,13 @@ import pygame
 
 #part A
 def threenp1(n):
-    count=0
-    while n > 1.0:
+    count = 0
+    while n > 1:
         count = count + 1
         if n % 2 == 0:
-            n=int(n/2)
+            n = int(n/2)
         else:
-            n = int(n*3+1)
+            n = int((n*3)+1)
     
     return count
 
@@ -19,16 +19,16 @@ def threenp1range(upper_limit):
     return objs_in_sequence
 
 def main():
-        pygame.init()
-        upper_limit=int(input("enter the limit number:"))
-        results = threenp1range(upper_limit)
-        graph_coordinates(results)
-        pygame.display.flip()
-        while True:
-            for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    pygame.quit()
-                    quit()
+    pygame.init()
+    upper_limit=int(input("enter the limit number:"))
+    results = threenp1range(upper_limit)
+    graph_coordinates(results)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
 
 
 #part B
@@ -36,31 +36,29 @@ def main():
 def graph_coordinates(threenplus1_iters_dict):
     window=pygame.display.set_mode ((600,600))
     window.fill("white")
-    coordinates = [(x,y) for x,y in threenplus1_iters_dict.items()]
+    coordinates = [(2*x,500-y) for (x,y) in threenplus1_iters_dict.items()]
 
     if len(coordinates)>=2:
-        pygame.draw.lines(window, "black", True, coordinates)
+        pygame.draw.lines(window, "black", False, coordinates)
 
 
-    new_display = pygame.transform.flip(window, False, True)
-    width, height = new_display.get_size()
-    new_display = pygame.transform.scale(window, (width * 5, height * 5))
-    window.blit(new_display, (10,10))
+    # new_display = pygame.transform.flip(window, False, True)
+    # (width, height) = new_display.get_size()
+    # new_display = pygame.transform.scale(window, (width * 5, height * 5))
+    # window.blit(new_display, (600,600))
 
 
-    iters_threenplus1_iters_dict = [tup[1] for tup in coordinates]
-    max_so_far = iters_threenplus1_iters_dict[1]
+    iters_threenplus1_iters_dict = [tup[1] for tup in threenplus1_iters_dict.items()]
+    print(iters_threenplus1_iters_dict)
 
-    for i in range(len(coordinates)):
-        if iters_threenplus1_iters_dict[i] > max_so_far:
-            max_so_far=iters_threenplus1_iters_dict[i]
+    # for i in range(len(coordinates)):
+    #     if iters_threenplus1_iters_dict[i] > max_so_far:
+    #         max_so_far=iters_threenplus1_iters_dict[i]
 
-    
-    font = pygame.font.Font(None, 5)
-    msg = font.render("max so far:", True, "green")
 
-    max_so_far = max(threenplus1_iters_dict)
-    msg = font.render("max_so_far:", True, ("green"))
+    max_so_far = max(iters_threenplus1_iters_dict)
+    font = pygame.font.Font(None, 50)
+    msg = font.render(f"max_so_far: {max_so_far}", True, ("green"))
     window.blit(msg, (10,10))
 
 main()
